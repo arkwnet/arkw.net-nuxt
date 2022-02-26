a46701f73738578cc843fbf02596488aa6571670
Sora Arakawa <mail@arkw.net>
2022-02-26 21:28:40 +0900

    Use two-space indent

--- components/Navi.vue
+++ components/Navi.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="navi">
-  	<div class="slider">
-  		<a href="https://arkw.net/products/pc_game/qubefall" target="blank"><div><img src="~/assets/img/header/md_qubefall.png"></div></a>
-  		<a href="https://arkw.net/products/web/prohaya/" target="blank"><div><img src="~/assets/img/header/md_prohaya.png"></div></a>
-  		<a href="https://arkw.net/products/web/dokaben/" target="blank"><div><img src="~/assets/img/header/md_dokaben.png"></div></a>
-  		<a href="https://psk.arkw.net/" target="blank"><div><img src="~/assets/img/header/md_psk.png"></div></a>
-  		<a href="https://arkw.net/products/web/kintetsu/" target="blank"><div><img src="~/assets/img/header/md_kin.png"></div></a>
-  		<a href="https://arkw.net/products/web/meitetsu/" target="blank"><div><img src="~/assets/img/header/md_mei.png"></div></a>
-  	</div>
+    <div class="slider">
+      <a href="https://arkw.net/products/pc_game/qubefall" target="blank">
+        <div><img src="~/assets/img/header/md_qubefall.png" /></div>
+      </a>
+      <a href="https://arkw.net/products/web/prohaya/" target="blank">
+        <div><img src="~/assets/img/header/md_prohaya.png" /></div>
+      </a>
+      <a href="https://arkw.net/products/web/dokaben/" target="blank">
+        <div><img src="~/assets/img/header/md_dokaben.png" /></div>
+      </a>
+      <a href="https://psk.arkw.net/" target="blank">
+        <div><img src="~/assets/img/header/md_psk.png" /></div>
+      </a>
+      <a href="https://arkw.net/products/web/kintetsu/" target="blank">
+        <div><img src="~/assets/img/header/md_kin.png" /></div>
+      </a>
+      <a href="https://arkw.net/products/web/meitetsu/" target="blank">
+        <div><img src="~/assets/img/header/md_mei.png" /></div>
+      </a>
+    </div>
   </div>
 </template>
