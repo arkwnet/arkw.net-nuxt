4dcf3528820d340c578d4ecacbcad681d35d108c
Sora Arakawa <mail@arkw.net>
2022-04-17 23:08:19 +0900

    レイアウトコンポーネントを使用

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <Navi />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header"
+import Navi from "../components/Navi"
+import Footer from "../components/Footer"
+export default {
+  components: {
+    Header,
+    Navi,
+    Footer,
+  },
+}
+</script>
