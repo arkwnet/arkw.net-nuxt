919f33f0c771aa29098ef3c98b40876e3ed47d32
Sora Arakawa <mail@arkw.net>
2022-04-25 18:56:40 +0900

    Linkコンポーネントを追加

---
+++ components/Link.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="link-banner">
+    <a :href="href" target="blank">
+      <img v-if="src != null" :src="image" :width="width" />
+      <span v-if="text != null">{{ text }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      require: true,
+      default: null,
+    },
+    src: {
+      type: String,
+      default: null,
+    },
+    width: {
+      type: String,
+      default: null,
+    },
+    text: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      image: "",
+    }
+  },
+  mounted() {
+    if (this.src != null) {
+      this.image = require("~/assets/img/links/" + this.src)
+    }
+  },
+}
+</script>
