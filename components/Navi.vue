81bb9216ca1b07915fbb4e83013ec65fc34135fc
Sora Arakawa <mail@arkw.net>
2022-05-11 12:27:45 +0900

    スライドショー画像を変更

--- components/Navi.vue
+++ components/Navi.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="navi">
     <div class="slider">
+      <NaviImage image="arkw_lab.png" />
       <NaviImage
         url="https://arkw.net/products/pc_game/qubefall/"
         image="qubefall.png"
       />
-      <NaviImage image="dummy_red.png" />
     </div>
   </div>
 </template>
