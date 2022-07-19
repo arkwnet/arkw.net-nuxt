59605d20d0128db1fe546c4a533ecb20a4eb27f3
Sora Arakawa <mail@arkw.net>
2022-07-20 00:48:40 +0900

    ヘッダー画像を一部差し替え

--- components/Navi.vue
+++ components/Navi.vue
@@ -2,10 +2,13 @@
   <div class="navi">
     <div class="slider">
       <NaviImage src="arkw_lab.png" />
-      <NaviImage src="arkw_lab_20220601.png" />
       <NaviImage
         href="https://arkw.net/products/win/LTTimer/"
         src="lt_timer.png"
+      />
+      <NaviImage
+        href="https://arkw.net/products/pc_game/qubefall/"
+        src="qubefall.png"
       />
     </div>
   </div>
