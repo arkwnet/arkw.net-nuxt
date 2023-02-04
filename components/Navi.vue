9785335270fbb0d88a475329d9cd4c78f1376737
Sora Arakawa <mail@arkw.net>
2023-02-04 21:43:34 +0900

    Naviのライブラリをhooperへ変更 (wip)

--- components/Navi.vue
+++ components/Navi.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="navi">
-    <div class="slider">
-      <NaviImage src="arkw_lab.png" />
-      <NaviImage
-        href="https://arkw.net/products/pc_game/hiragana_erase/"
-        src="hiragana_erase.png"
-      />
-      <NaviImage
-        href="https://arkw.net/products/pc_game/qubefall/"
-        src="qubefall.png"
-      />
-      <NaviImage
-        href="https://arkw.net/products/web/prohaya/"
-        src="prohaya.png"
-      />
-    </div>
+    <hooper :progress="true" :auto-play="true" :play-speed="4000">
+      <slide><NaviImage src="arkw_lab.png" /></slide>
+      <slide><NaviImage src="hiragana_erase.png" /></slide>
+      <slide><NaviImage src="qubefall.png" /></slide>
+      <slide><NaviImage src="prohaya.png" /></slide>
+      <!--
+      <slide>
+        <NaviImage
+          href="https://arkw.net/products/pc_game/hiragana_erase/"
+          src="hiragana_erase.png"
+        />
+      </slide>
+      <slide>
+        <NaviImage
+          href="https://arkw.net/products/pc_game/qubefall/"
+          src="qubefall.png"
+        />
+      </slide>
+      <slide>
+        <NaviImage
+          href="https://arkw.net/products/web/prohaya/"
+          src="prohaya.png"
+        />
+      </slide>
+      -->
+    </hooper>
   </div>
 </template>
 
 <script>
-import NaviImage from "./NaviImage"
+import { Hooper, Slide } from "hooper"
+import NaviImage from "./NaviImage.vue"
+import "../assets/css/hooper.css"
 export default {
   components: {
+    Hooper,
+    Slide,
     NaviImage,
   },
 }
