5fcf14ab79e7575155cf7e713e48a7c2d3cbe076
Sora Arakawa <mail@arkw.net>
2023-02-14 16:37:30 +0900

    Hooperのpropsを修正

--- components/Navi.vue
+++ components/Navi.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="navi">
-    <hooper :progress="true" :auto-play="true" :play-speed="4000">
+    <hooper
+      :auto-play="true"
+      :infinite-scroll="true"
+      :keys-control="false"
+      :play-speed="4000"
+      :progress="true"
+      :wheel-control="false"
+    >
       <slide><NaviImage src="arkw_lab.png" /></slide>
       <slide>
         <NaviImage
