2fb7d19ea2ad11bb4248ad92af4675bf5a60e8f0
Sora Arakawa <mail@arkw.net>
2023-03-22 20:01:18 +0900

    言語選択メニュー追加 (wip)

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="header">
-    <div class="wrapper">
-      <a href="/">
-        <img src="~/assets/img/logo.svg" />
-      </a>
-      <div class="icon">
-        <div class="language">日本語</div>
+  <div>
+    <div class="header">
+      <div class="wrapper">
+        <a href="/">
+          <img src="~/assets/img/logo.svg" />
+        </a>
+        <div class="icon">
+          <div class="language">日本語</div>
+        </div>
       </div>
     </div>
+    <ul class="menu language">
+      <li>日本語</li>
+      <li>English</li>
+      <li>简体中文</li>
+    </ul>
   </div>
 </template>
