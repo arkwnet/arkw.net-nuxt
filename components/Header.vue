8eb899135c3c0187c408b556a54395bc3239d36d
Sora Arakawa <mail@arkw.net>
2023-03-27 22:08:40 +0900

    vue-click-outsideをインストール

--- components/Header.vue
+++ components/Header.vue
@@ -6,22 +6,28 @@
           <img src="~/assets/img/logo.svg" />
         </a>
         <div class="icon">
-          <div class="language" @click="onLanguageMenu()">
-            {{ $t("language") }}
+          <div v-click-outside="hideLanguageMenu">
+            <div class="language" @click="onLanguageMenu()">
+              {{ $t("language") }}
+            </div>
+            <ul v-if="isLanguageMenu" class="menu language">
+              <li @click="setLanguage('ja')">日本語</li>
+              <li @click="setLanguage('en')">English</li>
+              <li @click="setLanguage('zh')">简体中文</li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
-    <ul class="menu language" v-if="isLanguageMenu">
-      <li @click="setLanguage('ja')">日本語</li>
-      <li @click="setLanguage('en')">English</li>
-      <li @click="setLanguage('zh')">简体中文</li>
-    </ul>
   </div>
 </template>
 
 <script>
+import ClickOutside from "vue-click-outside"
 export default {
+  directives: {
+    ClickOutside,
+  },
   data() {
     return {
       isLanguageMenu: false,
@@ -31,16 +37,22 @@ export default {
     onLanguageMenu() {
       switch (this.isLanguageMenu) {
         case false:
-          this.isLanguageMenu = true
+          this.showLanguageMenu()
           break
         case true:
-          this.isLanguageMenu = false
+          this.hideLanguageMenu()
           break
       }
     },
+    showLanguageMenu() {
+      this.isLanguageMenu = true
+    },
+    hideLanguageMenu() {
+      this.isLanguageMenu = false
+    },
     setLanguage(lang) {
       this.$router.push(this.switchLocalePath(lang))
-      this.isLanguageMenu = false
+      this.hideLanguageMenu()
     },
   },
 }
