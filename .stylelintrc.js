fc8e5ff73a99bf28e81dcd3dae3f4bdfccb47805
Sora Arakawa <mail@arkw.net>
2023-09-14 12:17:38 +0900

    Stylelintの導入

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,19 +1,4 @@
 module.exports = {
-  plugins: ["stylelint-prettier"],
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-prettier",
-    "stylelint-config-standard-scss",
-    "stylelint-config-recess-order",
-  ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
-  ignoreFiles: [
-    "static/css/slick.css",
-    "static/css/slick-theme.css",
-    "components/*.vue",
-    "pages/*.vue",
-  ],
-  customSyntax: "postcss-html",
+  extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
+  ignoreFiles: ["components/*.vue", "pages/*.vue"],
 }
